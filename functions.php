0355708cd7a8eabb6a7567662a8a1c4dc914b675
mailgregcarlson <mailgregcarlson@gmail.com>
2018-09-07 08:48:09 -0500

    finished up theme test

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'twentysixteen-style'; // This is 'twentyseventeen-style' for the Twenty Seventeen theme.
+    $parent_style = 'twentysixteen-style'; // This is 'twentysixteen-style' for the Twenty Sixteen theme.
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
@@ -9,10 +9,79 @@ function my_theme_enqueue_styles() {
         array( $parent_style ),
         wp_get_theme()->get('Version')
     );
+
 }
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
+function contactFormAirtable (){
 
+  if($_POST['action'] == 'contact_form_airtable'){
+
+    //post vars
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $comments = $_POST['comments'];
+
+    //insert records for airtable
+    define("CONFIG_ID", 0);
+    define("CONFIG_NAME", "Default");
+
+    $new_contact_form = array("First Name" => $firstName, "Last Name" => $lastName, "Email" => $email, "Phone Number" => $tel,
+                              "Address" => $address, "City" => $city, "State" => $state, "Zip" => $zip,
+                              "Comments" => $comments);
+
+    $contact_form_data = AirpressConnect::create(CONFIG_ID, "Contact Submits", $new_contact_form);
+
+    $query = new AirpressQuery("Contact Submits", CONFIG_ID);
+    $collection = new AirpressCollection($query, false);
+    $collection->setRecords(array($contact_form_data));
+
+    $contact_form_record = $collection[0];
+
+    $contact_form_id = $contact_form_recrod->record_id();
+
+    if($contact_form_id > 0){
+
+      //email airtable users to notify record has been created
+      $subject = 'Contact Form: '.$reason.' - '.$_POST['name'];
+      $headers = 'From: My Blog Contact Form <contact@me.com>';
+      $send_to = "airtableContact@airtabletest.com";
+      $subject = "Airtable Record Created Contact Form";
+      $message = "A record has been created in the Contact Form Submits Tables for Airtable";
+
+      if (wp_mail($send_to, $subject, $message, $headers)) {
+        echo json_encode(array('status' => 'success', 'message' => 'Contact message sent.'));
+        exit;
+      } else {
+        throw new Exception('Failed to send email.');
+      }
+      } catch (Exception $e) {
+        echo json_encode(array('status' => 'error', 'message' => $e->getMessage()));
+        exit;
+      }
+    }
+
+    $results = array("status"=>"success", "contactFormId"=>$contact_form_id);
+
+    $results = array("status"=>"success", "firstName"=>$firstName);
+
+    echo json_encode($results);
+
+  }
+
+  die();
+
+}
+
+add_action('wp_ajax_contact_form_airtable', 'contactFormAirtable');
+add_action('wp_ajax_nopriv_contact_form_airtable', 'contactFormAirtable');
 
 function sendContactFormToSiteAdmin () {
   try {
--- js/contact-form.js
+++ js/contact-form.js
@@ -1,47 +1,100 @@
 jQuery(document).ready(function ($) {
-  alert("hello there!");
-    var is_sending = false,
-        failure_message = 'Whoops, looks like there was a problem. Please try again later.';
 
+    //airtable record contact form
     $('#contact-form').submit(function (e) {
-      if (is_sending || !validateInputs()) {
-        return false; // Don't let someone submit the form while it is in-progress...
+
+      if (!validateInputs()) {
+        return false;
       }
-      e.preventDefault(); // Prevent the default form submit
-      var $this = $(this); // Cache this
+
+      e.preventDefault();
+
+      var $this = $(this);
+      $data = $(this).serializeArray();
+      var dataObj = new Object();
+      for($i = 0; $i < $data.length; $i++){
+
+        var inputName = $data[$i]['name'];
+        var inputValue = $data[$i]['value'];
+
+        if(inputName === "action" || inputName === "email" ||
+          inputName === "firstName" || inputName === "lastName" ||
+          inputName === "tel" || inputName === "address"||
+          inputName === "city" || inputName === "state" ||
+          inputName === "zip" || inputName === "comments"){
+              dataObj[inputName] = inputValue;
+        }
+      }
+
+      var jsonObj = JSON.stringify(dataObj);
+
       $.ajax({
-        url: '<?php echo admin_url("admin-ajax.php") ?>', // Let WordPress figure this url out...
-        type: 'post',
-        dataType: 'JSON', // Set this so we don't need to decode the response...
-        data: $this.serialize(), // One-liner form data prep...
-        beforeSend: function () {
-          is_sending = true;
-          // You could do an animation here...
-        },
-        error: handleFormError,
+        type: 'POST',
+        url: ContactFormAjax.ajaxurl,
+        dataType: 'json',
+        data: JSON.parse(jsonObj),
         success: function (data) {
           if (data.status === 'success') {
-            // Here, you could trigger a success message
+            window.location.href = "/thank-you-submit?record_id="+data.record_id;
           } else {
-            handleFormError(); // If we don't get the expected response, it's an error...
+            alert("error");
+            handleFormError();
           }
-        }
+        },
+        error: function(MLHttpRequest, textStatus, errorThrown){
+        alert("There was an error: " + errorThrown);
+     },
       });
     });
 
-    function handleFormError () {
-      is_sending = false; // Reset the is_sending var so they can try again...
-      alert(failure_message);
+    function validateInputs () {
+
+      var submitForm = true;
+      var firstName = $('#contact-form > input[name="firstName"]'),
+          lastName = $('#contact-form > input[name="lastName"]'),
+          email = $('#contact-form > input[name="email"]'),
+          phoneNumber = $('#contact-form > input[name="tel"]');
+          zipcode = $('#contact-form > input[name="zip"]');
+
+          $message = $('#contact-form > textarea').val();
+      if (!firstName.val()) {
+          $(firstName).after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter first name</div>").focus();
+          submitForm = false;
+      }
+      if (!lastName.val()) {
+          $(lastName).after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter last name</div>").focus();
+          submitForm = false;
+      }
+      if (!isEmailAddressValid(email.val())) {
+          $(email).after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter valid email address</div>").focus();
+          submitForm = false;
+      }
+
+      if (!isPhoneNumberValid(phoneNumber.val())) {
+          $(phoneNumber).after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter valid phone number (123-456-7890)</div>").focus();
+          submitForm = false;
+      }
+
+      if (!isZipCodeValid(zipcode.val()) && zipcode.val()) {
+          $(zipcode).after("<div class='validation' style='color:red;margin-bottom: 20px;'>Please enter valid zip code (66044)</div>").focus();
+          submitForm = false;
+      }
+
+      return submitForm;
     }
 
-    function validateInputs () {
-      var $name = $('#contact-form > input[name="name"]').val(),
-          $email = $('#contact-form > input[name="email"]').val(),
-          $message = $('#contact-form > textarea').val();
-      if (!$name || !$email || !$message) {
-        alert('Before sending, please make sure to provide your name, email, and message.');
-        return false;
-      }
-      return true;
+    function isZipCodeValid(zipcode){
+      var pattern = /[0-9]{5}/;
+      return pattern.test(zipcode);
+    }
+
+    function isPhoneNumberValid(phoneNumber){
+      var pattern = /[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+      return pattern.test(phoneNumber);
+    }
+
+    function isEmailAddressValid(email){
+      var pattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+      return pattern.test(email);
     }
 });
