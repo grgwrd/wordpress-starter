be9da7e6ea0b06650a08ca93df49260734cd593d
mailgregcarlson <mailgregcarlson@gmail.com>
2018-09-07 08:56:01 -0500

    moved wp hook

--- functions.php
+++ functions.php
@@ -12,7 +12,14 @@ function my_theme_enqueue_styles() {
 
 }
 
+// embed the javascript file that makes the AJAX request
+wp_enqueue_script( 'contact_form_airtable','/wp-content/themes/custom/twentysixteen-child/js/contact-form.js', array( 'jquery' ) );
+
+// declare the URL to the file that handles the AJAX request (wp-admin/admin-ajax.php)
+wp_localize_script( 'contact_form_airtable', 'ContactFormAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 
 function contactFormAirtable (){
 
@@ -82,35 +89,6 @@ function contactFormAirtable (){
 
 add_action('wp_ajax_contact_form_airtable', 'contactFormAirtable');
 add_action('wp_ajax_nopriv_contact_form_airtable', 'contactFormAirtable');
-
-function sendContactFormToSiteAdmin () {
-  try {
-    if (empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message'])) {
-      throw new Exception('Bad form parameters. Check the markup to make sure you are naming the inputs correctly.');
-    }
-    if (!is_email($_POST['email'])) {
-      throw new Exception('Email address not formatted correctly.');
-    }
-
-    $subject = 'Contact Form: '.$reason.' - '.$_POST['name'];
-    $headers = 'From: My Blog Contact Form <contact@me.com>';
-    $send_to = "mailgregcarlson@gmail.com";
-    $subject = "MyBlog Contact Form ($reason): ".$_POST['name'];
-    $message = "Message from ".$_POST['name'].": \n\n ". $_POST['message'] . " \n\n Reply to: " . $_POST['email'];
-
-    if (wp_mail($send_to, $subject, $message, $headers)) {
-      echo json_encode(array('status' => 'success', 'message' => 'Contact message sent.'));
-      exit;
-    } else {
-      throw new Exception('Failed to send email. Check AJAX handler.');
-    }
-  } catch (Exception $e) {
-    echo json_encode(array('status' => 'error', 'message' => $e->getMessage()));
-    exit;
-  }
-}
-add_action("wp_ajax_contact_send", "sendContactFormToSiteAdmin");
-add_action("wp_ajax_nopriv_contact_send", "sendContactFormToSiteAdmin");
 
 
 ?>
